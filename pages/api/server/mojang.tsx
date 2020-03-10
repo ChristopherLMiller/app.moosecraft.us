bc8151182aaba97d070d1224c55c4db1c71183b1
ChristopherLMiller <chris@christopherleemiller.me>
2020-03-10 07:45:48 -0400

    fix server status, fixes console error

--- pages/api/server/mojang.tsx
+++ pages/api/server/mojang.tsx
@@ -1,8 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import fetch from 'isomorphic-unfetch';
 
-const minestat = require('../../../src/minestat');
-
 export default async ( req: NextApiRequest, res: NextApiResponse ) => {
     // grab the data from mojang
     const response = await fetch('https://status.mojang.com/check');
@@ -11,16 +9,18 @@ export default async ( req: NextApiRequest, res: NextApiResponse ) => {
     // setup some variables to figure out which server person wants
     const server = req.query?.address;
     
+    // set a default color
+    let status = 'grey';
+
     // loop over the servers trying to find this one
     data.forEach((element: object) => {
-         if (Object.keys(element)[0] === server) {
-            const key = Object.keys(element);
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'Application/Json');
-            res.end(JSON.stringify({status: Object.values(element)[0]}));
-         }
+        
+        if (Object.keys(element)[0] === server) {
+            status = Object.values(element)[0];    
+        }
     });
 
-    res.statusCode = 401;
-    res.end('Unable to find');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'Application/Json');
+    res.end(JSON.stringify({status: status}));
 }
--- pages/api/server/status.tsx
+++ pages/api/server/status.tsx
@@ -1,11 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const minestat = require('../../../src/minestat');
+const minestat = require('../../../src/utils/minestat');
 
 export default ( req: NextApiRequest, res: NextApiResponse ) => {
+    // defualt colors
+    let status = 'grey';
+
     minestat.init(req.query?.address || 'localhost', req.query?.port || 25565, () => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({status: minestat.online ? 'green' : 'red'}));    
-    });  
+        console.log(minestat.online);
+        status = minestat.online ? 'green' : 'red';           
+    }); 
+    
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({status: status}));
 }
