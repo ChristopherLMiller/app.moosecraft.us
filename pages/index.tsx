1b053e1bb6b02b37bc01c9d5452769e53335f085
ChristopherLMiller <chris@christopherleemiller.me>
2020-03-10 08:08:02 -0400

    Create home page beginning

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,53 @@
-import { Page } from "../src/components/layout/Page";
-import { PageTitle } from "../src/components/elements/PageTitle";
+import styled from "styled-components"
+import Link from "next/link";
+
+const StyledHomePage = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: url("https://static.moosecraft.us/v2/img/hero-home.png") no-repeat;
+  background-size: cover;
+`;
+
+const Sign = styled.div`
+background: url(https://static.moosecraft.us/v2/img/sign.png) no-repeat;
+background-size: cover;
+left: 95px;
+padding-top: 425px;
+position: absolute;
+top: -185px;
+/*-670px;*/
+transform: rotate(-13deg);
+transition: all 1s ease 0s;
+width: 552px;
+`;
+
+const FlexBox = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const ImgLink = styled.img`
+  width: 450px;
+  margin-bottom: 80px;
+  transition: all 0.25s;
+
+  :hover {
+    transform: rotate3d(0, 0, 1, -13deg);
+  }
+`;
 
 const Home = () => (
-  <Page>
-    <PageTitle>Welcome To Moosecraft!</PageTitle>
-  </Page>
+  <StyledHomePage>
+    <Sign>
+      <FlexBox>
+        <Link href="/">
+          <a>
+            <ImgLink src="https://static.moosecraft.us/v2/img/logo.png" />
+          </a>
+        </Link>
+      </FlexBox>
+    </Sign>
+  </StyledHomePage>
 );
 
 
