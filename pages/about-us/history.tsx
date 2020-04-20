1d7b5096f303001517a11293f5164f29769f0316
ChristopherLMiller <chris@christopherleemiller.me>
2020-04-20 04:33:51 -0400

    Style History Page

--- pages/about-us/history.tsx
+++ pages/about-us/history.tsx
@@ -7,9 +7,18 @@ const HistoryMarkdown = require('../../data/markdown/history.md');
 
 const StyledContent = styled.div`
     font-size: var(--p-responsive);
-    background: var(--color-grey-light);
+    background: var(--color-blue-opposite);
     padding: 20px;
     box-shadow: var(--box-shadow);
+
+    color: var(--color-brown);
+
+    h2 {
+        text-decoration: underline;
+        text-align: center;
+        color: var(--color-green-dark);
+    }
+
 `;
 
 const HistoryPage = () => (
